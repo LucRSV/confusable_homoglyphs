a9054dda71939f175f84ef21ad79a7065a8ecfbf
= <=>
2018-03-01 16:26:07 -0800

    added coersion test

---
+++ test_coerce.py
@@ -0,0 +1,35 @@
+from confusable_homoglyphs import confusables
+
+def is_valid_int(char):
+    try:
+        return isinstance(int(char), int)
+    except:
+        return False
+
+def string_coerce(string):
+    string_chars = []
+    for char in string:
+        aliases = confusables.is_confusable(char, greedy=False, preferred_aliases=[], allow_digit=False)
+        string_chars += aliases
+
+    coerced = []
+    for char in string_chars:
+        print(char)
+        if char['alias'] == 'LATIN':
+            coerced.append(char['character'])
+
+        elif is_valid_int(char):
+            coerced.append(char['character'])
+
+        else:
+            for homoglyph in char['homoglyphs']:
+                if homoglyph['n'].startswith('LATIN') or homoglyph['n'].startswith('DIGIT'):
+                    coerced.append(homoglyph['c'])
+                    break
+
+    return ''.join(coerced)
+
+if __name__ == '__main__':
+    test_str = '𐌚cha𝐧'
+    result = string_coerce(test_str)
+    print('{} -> {}'.format(test_str, result))
